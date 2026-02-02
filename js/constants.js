431c9fd00ff2b0e2fb3df3c0baf6d2e248d07554
potermaus75-collab <potermaus75@gmail.com>
2026-02-02 10:01:10 +0900

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,8 @@
+export const TILE_SIZE = 55;
+export const RAW_MAP = [
+    { name:"출발", type:"start", cost:0 },
+    // ... 기존 40개 맵 데이터
+];
 
+export const COST_RATIO = { land:1, villa:0.5, building:1.0, hotel:2.0, landmark:4.0 };
+export const TOLL_RATIO = { land:0.5, villa:1.5, building:3.0, hotel:6.0, landmark:15.0 };
